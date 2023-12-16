84394f09d5b83e6b5b80a00e835c35344fed0f19
nath-hub <nathalietaffot@gmail.com>
2023-12-16 12:13:49 +0100

    - add middleware

---
+++ app/Http/Middleware/CaptureRequests.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CaptureRequests
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        dd($next($request));
+
+        return response()->json(['message' => "Request bien capturee"]);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware'=>'CaptureRequest'], function (){
+    
+    Route::apiResource('users', UserController::class);
+
 });
 
     /*
